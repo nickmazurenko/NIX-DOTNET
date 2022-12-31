5e3e458fae8b86144db48db22871aa3d5ff0bc19
nickmazurenko <surprisedsunrise@gmail.com>
2022-12-31 18:32:53 +0200

    incapsulate animal class properties

--- Task_1/ZooData/Models/Animal.cs
+++ Task_1/ZooData/Models/Animal.cs
@@ -4,10 +4,28 @@ namespace ZooData.Models;
 
 public class Animal
 {
-    public AnimalType Type = AnimalType.Mammal;
-    public string? Name;
-    public string? Description;
+    private AnimalType _type = AnimalType.Mammal;
+    private string? _name;
+    private string? _description;
     private DateTime _birthDate = DateTime.Now;
+
+    public AnimalType Type
+    {
+        get => _type;
+        set => _type = value is AnimalType ? value : AnimalType.Mammal;
+    }
+
+    public string? Name
+    {
+        get => _name;
+        set => _name = value is { Length: > 0 } ? value : "unknown";
+    }
+
+    public string? Description
+    {
+        get => _description;
+        set => _description = value is { Length: > 0 } ? value : "unknown";
+    }
 
     public string? BirthDate
     {
@@ -22,7 +40,12 @@ public class Animal
         }
     }
 
-    public int Age => DateTime.Now.Year - _birthDate.Year;
+    private int Age => DateTime.Now.Year - _birthDate.Year;
+
+    public void ChangeAge(int newAge)
+    {
+        BirthDate = $"{DateTime.Now.Day}.{DateTime.Now.Month}.{DateTime.Now.Year - newAge}";
+    }
 
     public override string ToString()
     {
